1377c62438bfce43845e6756acf74d2b8afde0ae
Michael Small <michaelsmalldev@gmail.com>
2026-05-22 12:18:53 -0500

    chore: add shell for signal forms comp

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,14 +1,18 @@
 import { Component } from '@angular/core';
 import { AngularAria } from './topics/angular-aria';
+import { SignalForms } from './topics/signal-forms';
 
 @Component({
   selector: 'app-root',
-  imports: [AngularAria],
+  imports: [AngularAria, SignalForms],
   template: `
     <h1>v22</h1>
 
     <h2>Angular Aria package (<code>@angular/aria</code>): Stable</h2>
     <app-angular-aria />
+
+    <h2>Signal Forms package: Stable</h2>
+    <app-signal-forms />
   `,
 })
 export class App {}
---
+++ src/app/topics/signal-forms.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-signal-forms',
+  template: `
+    <p><a href="https://angular.dev/guide/forms/signals/overview" target="_blank">Docs</a></p>
+    <em>TODO: add examples</em>
+  `,
+})
+export class SignalForms {}
