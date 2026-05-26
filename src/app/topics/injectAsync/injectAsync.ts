5d08dc033556e81549a7db141cec8ff06cd4cf80
Michael Small <michaelsmalldev@gmail.com>
2026-05-25 19:31:41 -0500

    chore: convert `injectAsync` example to markdown

--- src/app/topics/injectAsync/injectAsync.ts
+++ src/app/topics/injectAsync/injectAsync.ts
@@ -1,27 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component, injectAsync } from '@angular/core';
 import { MarkdownModule } from 'ngx-markdown';
 
 @Component({
   selector: 'app-injectAsync',
-  imports: [MarkdownModule],
-  template: `
-    <p>TODO - point to live v22 documentation, also for citation</p>
-    <p>
-      <a href="https://next.angular.dev/guide/di/lazy-loading-services#" target="_blank"
-        >Lazy Loading Services Guide</a
-      >
-    </p>
-
-    <markdown [data]="quoteDefinition" />
-
-    <p>
-      Docs: "Imagine a ReportExporter that depends on a heavy spreadsheet library. Most users open
-      the report; only a few click Export. Load the exporter on demand:". Once you click this, check
-      your console logs. Not only will the message that I added to the respective method call be
-      logged, but you will also see a log from the service's constructor that was created on demand.
-    </p>
-    <button (click)="export()">Export</button>
-  `,
+  imports: [MarkdownModule, CommonModule],
+  templateUrl: './injectAsync.html',
+  preserveWhitespaces: true,
 })
 export class InjectAsync {
   private exporter = injectAsync(() => import('./lazy-service').then((m) => m.LazyService));
@@ -30,12 +15,4 @@ export class InjectAsync {
     const exporter = await this.exporter();
     exporter.export();
   }
-
-  protected quoteDefinition = `> Angular's \`injectAsync\` function lets you load a service on demand, only when it's actually
-        needed. This is useful when a service depends on a large library or rarely used feature, and
-        you don't want to pay for it on the initial page load. When you use injectAsync, the
-        service's code is split out by your bundler into a separate JavaScript chunk and downloaded
-        the first time you ask for the instance. Once loaded, Angular resolves the service through
-        the regular DI system, so it can still depend on other injectables and behaves like any
-        other singleton. - [Docs](https://next.angular.dev/guide/di/lazy-loading-services#)`;
 }
