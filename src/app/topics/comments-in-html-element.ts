59fd87c4763175a2d18d4be6bd2cbc638a11d62f
Michael Small <michaelsmalldev@gmail.com>
2026-06-02 18:20:21 -0500

    chore: add context for comments in html elements

--- src/app/topics/comments-in-html-element.ts
+++ src/app/topics/comments-in-html-element.ts
@@ -3,12 +3,15 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-comments-in-html-element',
   template: `
-    <p>TODO - update with PR's example once language service supports this officially</p>
+    <p>
+      <a href="https://www.youtube.com/shorts/TPsNfRc-2yk" target="_blank">Igor video</a>
+    </p>
     <p>
       <a href="https://github.com/angular/angular/pull/67179" target="_blank"
         >PR feat(compiler): Support comments in html elements</a
       >
     </p>
+    <p>Some tooling support pending, hence no demo for the stream</p>
   `,
 })
 export class CommentsInHtmlElement {}
